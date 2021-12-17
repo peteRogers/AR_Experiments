8f338f8f1f5e070fe92d1884395db81126ce16c9
Peter Rogers <p.rogers@gold.ac.uk>
2021-12-17 14:18:02 +0000

    Initial Commit

---
+++ AR_Experiments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AR_Experiments
+//
+//  Created by Peter Rogers on 17/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
